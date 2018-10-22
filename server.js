e743b6de4b5ecabc865370b9002e25239ac46225
Fikayo <fik4christ@yahoo.com>
2018-10-22 06:15:06 +0100

    Authorization Setup

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require("express");
+var jwt = require("express-jwt");
+var jwks = require("jwks-rsa");
+const env = require("./env");
 
 // App
 const app = express();
@@ -6,6 +9,20 @@ const app = express();
 // Set port
 const port = process.env.PORT || "1337";
 app.set("port", port);
+
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${env.auth0Domain}/.well-known/jwks.json`
+  }),
+  audience: env.apiIdentifier,
+  issuer: `https://${env.auth0Domain}/`,
+  algorithms: ["RS256"]
+});
+
+app.use(jwtCheck);
 
 // Routes
 require("./routes")(app);
